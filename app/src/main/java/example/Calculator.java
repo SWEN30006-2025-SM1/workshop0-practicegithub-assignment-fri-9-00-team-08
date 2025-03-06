b3a6b208d1d388c838cbc3f2d10bfbc8315008ce
yuanhaog1 <yuanhao.gao@student.unimelb.edu.au>
2025-03-07 10:47:18 +1100

    second edition

--- app/src/main/java/example/Calculator.java
+++ app/src/main/java/example/Calculator.java
@@ -2,22 +2,22 @@ package example;
 
 public class Calculator {
     public int add(int a, int b) {
-        // TODO: Implement this method
+        // this is an add method to add 2 integers together
         return a + b;
     }
 
     public int minus(int a, int b) {
-        // TODO: Implement this method
+        // integer a minus integer b
         return a - b;
     }
 
     public int multiply(int a, int b) {
-        // TODO: Implement this method
+        // a *b
         return a * b;
     }
 
     public double divide(int a, int b) {
-        // TODO: Implement this method
+        // a /b
         return a / b;
     }
 }
